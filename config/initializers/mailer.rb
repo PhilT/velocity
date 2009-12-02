00afac800e8999652865f94bc79ef325832ce5f3
Phil Thompson <phil.thompson@puresolo.com>
2009-12-02 17:04:09 +0000

    Added mailer settings. Styling changes

---
+++ config/initializers/mailer.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+  :address => "mail.authsmtp.com",
+  :port => 25,
+  :domain => "puresolo.com",
+  :user_name => "ac35942",
+  :password => "qjur7rbaa",
+  :authentication => :plain
+}
+
