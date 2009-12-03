de518e0c545e25df469bd8d1ab9593c730499214
Phil Thompson <phil@electricvisions.com>
2009-12-03 01:43:12 +0000

    Added velocity figure to main tasks page. Fixed some styling issues on releases and tasks page

--- app/models/release.rb
+++ app/models/release.rb
@@ -2,6 +2,7 @@ class Release < ActiveRecord::Base
   has_many :tasks
 
   named_scope :previous, :conditions => 'finished_at IS NOT NULL', :order => 'finished_at DESC'
+  named_scope :last, :conditions => 'finished_at IS NOT NULL', :order => 'finished_at DESC', :limit => 1
 
   def self.current
     find_by_finished_at(nil)
@@ -17,5 +18,10 @@ class Release < ActiveRecord::Base
     touch :finished_at
     Release.create!
   end
+
+  def self.velocity
+    last[0].tasks.features.count rescue '(none)'
+  end
+
 end
 
