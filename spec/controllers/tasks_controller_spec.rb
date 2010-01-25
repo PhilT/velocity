7f7eb0c6dd9148c0bee3dda814df43e6e069b828
Phil <phil@phil-laptop.(none)>
2010-01-25 17:00:35 +0000

    more testing

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -4,14 +4,37 @@ describe TasksController do
 
   before(:each) do
     activate_authlogic
-    UserSession.create(Factory(:developer))
+    @logged_in_user = Factory(:developer)
+    UserSession.create(@logged_in_user)
+    @task = Factory(:task)
   end
 
   describe 'update' do
     it 'should mark task invalid with current user when state invalid' do
-      task = Factory(:task)
-      put :update, :id => task.id, :state => 'invalid'
+      put :update, :id => @task.id, :state => 'invalid'
       assigns[:task].state.should == 'invalid'
+      assigns[:task].assigned.should == @logged_in_user
+      assigns[:moved].should == nil
+    end
+
+    it 'should move invalid task to current release' do
+      task = Factory(:future_task)
+      put :update, :id => task.id, :state => 'invalid'
+      assigns[:task].release.should == Release.current
+      assigns[:moved].should == true
+    end
+
+    it 'should mark a pending task as started when in current release' do
+      put :update, :id => @task.id
+      assigns[:task].state.should == 'started'
+      assigns[:moved].should == nil
+    end
+
+    it 'should move a task to the current release when not in it' do
+      task = Factory(:future_task)
+      put :update, :id => task.id
+      assigns[:task].release.should == Release.current
+      assigns[:moved].should == true
     end
   end
 
--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -26,20 +26,14 @@ end
 Factory.define :task do |f|
   f.name 'This is a task'
   f.association :author, :factory => :customer
+  f.release {Release.current}
+  f.state 'pending'
 end
 
-## This will copy the attributes from the parent factory to the current one with an ability to override them.
-#Factory.define :super_user, :parent => :user do |u|
-#  u.super_user true
-#end
-
-## Associations
-#Factory.define :post do |p|
-#  p.author {|author| author.association(:user, :last_name => 'Writely') }
-#end
-
-## Or in short form
-#Factory.define :post do |p|
-#  p.association :author, :factory => :user
-#end
+Factory.define :future_task, :class => Task do |f|
+  f.name 'This is a task'
+  f.association :author, :factory => :customer
+  f.release nil
+  f.state 'pending'
+end
 
