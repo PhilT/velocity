ce79e0276e01749a4473fc3038da64b7e08119c9
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2010-02-04 20:53:15 +0000

    release has_many stories

--- app/models/story.rb
+++ app/models/story.rb
@@ -3,4 +3,9 @@ class Story < ActiveRecord::Base
 
   has_many :tasks
   has_and_belongs_to_many :stakeholders, :class_name => 'User', :foreign_key => :stakeholder_id
+  belongs_to :release
+
+  def state
+    'pending'
+  end
 end
