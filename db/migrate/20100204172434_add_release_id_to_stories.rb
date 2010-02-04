12e9b883443f7764269bb14037efa475cc6d4b05
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2010-02-04 20:53:15 +0000

    release has_many stories

---
+++ db/migrate/20100204172434_add_release_id_to_stories.rb
@@ -0,0 +1,9 @@
+class AddReleaseIdToStories < ActiveRecord::Migration
+  def self.up
+    add_column :stories, :release_id, :integer
+  end
+
+  def self.down
+    remove_column :stories, :release_id
+  end
+end
