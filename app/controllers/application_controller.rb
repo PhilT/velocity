b4cdb3479c96dd426868f8195bac4a75cf75e8a5
Hanna Kalinowska <hanna.kalinowska@puresolo.com>
2010-04-14 10:25:59 +0100

    don't display bugs with stories in bugs list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
 
   def find_stuff
     @stories = Story.current
-    @bugs = Task.current.bugs
+    @bugs = Task.current.bugs.without_story
     @future_tasks = Task.current.without_story - @bugs
     @developers = User.developers
     @non_developers = User.non_developers
