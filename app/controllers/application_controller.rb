ddfb6d325f8dc282862b86b717df742874db61df
Phil Thompson <phil@electricvisions.com>
2010-04-28 03:43:18 +0100

    made groups and tasks added could not be dragged/dropped, merged separate future and bugs lists

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,7 @@ class ApplicationController < ActionController::Base
   def find_stuff
     @stories = Story.current
     @groups = @stories
-    @bugs = Task.current.bugs
-    @future_tasks = Task.current - @bugs
+    @tasks = Task.current
     @developers = User.developers
     @non_developers = User.non_developers
   end
--- public/javascripts/groups.js
+++ public/javascripts/groups.js
@@ -1,10 +1,18 @@
 $(function(){
-  $(".group").draggable({revertDuration: 300, revert: 'invalid', helper: 'clone'});
-  $(".task").droppable({
-    hoverClass: 'drop_here',
-    drop: function(event, ui) {
-      $.put('/tasks/' + $(this).attr('id').substr(5), {group_id: ui.draggable.attr('id').substr(6)});
-    }
-  });
+  $.fn.draggableGroup = function(){
+    $(this).draggable({revertDuration: 300, revert: 'invalid', helper: 'clone'});
+  }
+
+  $.fn.droppableTask = function(){
+    $(this).droppable({
+      hoverClass: 'drop_here',
+      drop: function(event, ui) {
+        $.put('/tasks/' + $(this).attr('id').substr(5), {group_id: ui.draggable.attr('id').substr(6)});
+      }
+    });
+  }
+
+  $(".group").draggableGroup();
+  $(".task").droppableTask();
 })
 
