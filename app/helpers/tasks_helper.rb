7cab017010e1dfaa7b0be12a8a7d048fb96f398b
Phil Thompson <phil@electricvisions.com>
2010-04-29 02:47:54 +0100

    worked on initial solution for releases

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -11,12 +11,16 @@ module TasksHelper
     f.submit task.action, :id => "task_#{task.id}_submit"
   end
 
-  def display_velocity(velocity)
-    "#{velocity.round} (features per week)"
-  end
-
   def select_list_for(task)
     task.story ? "#story_#{task.story_id} .tasks" : "#{task.bug? ? '#bugs' : '#future'} .tasks"
+  end
+
+  def release_full?(count)
+    if !@release_full && Release.velocity == (count + 1)
+      @release_full = true
+    else
+      false
+    end
   end
 end
 
