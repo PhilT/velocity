3ab4a5ff0ecdb90e522f4537abbb55510a659ec0
Phil Thompson <phil@electricvisions.com>
2010-04-29 04:36:41 +0100

    removed unused story methods and columns, removed finished_at from Release and corrected usage

--- app/models/release.rb
+++ app/models/release.rb
@@ -3,28 +3,21 @@ class Release < ActiveRecord::Base
   has_many :stories
   belongs_to :finished_by, :class_name => 'User', :foreign_key => :finished_by
 
-  named_scope :previous, :conditions => 'finished_at IS NOT NULL', :order => 'finished_at DESC'
+  named_scope :previous, :order => 'created_at DESC'
 
   delegate :features, :bugs, :refactorings, :to => :tasks
 
-  def finish!(user)
+  def self.finish!(user)
     return false unless can_complete_release?
-
+    release = Release.create(:finished_by => user)
     Task.current.without_story.verified.each do |task|
-      task.add_to_release!(self)
+      task.add_to_release!(release)
     end
     Task.current.without_story.invalid.each do |task|
-      task.add_to_release!(self)
-    end
-    Story.current.verified.each do |story|
-      story.add_to_release!(self)
-      story.tasks.each {|task| task.add_to_release!(self)}
+      task.add_to_release!(release)
     end
 
-    touch :finished_at
-    update_attribute :finished_by, user
-
-    ReleaseMailer.deliver_release_notification(User.all, self)
+    ReleaseMailer.deliver_release_notification(User.all, release)
   end
 
   def self.velocity
@@ -33,7 +26,7 @@ class Release < ActiveRecord::Base
 
   def velocity
     begin
-      releases = Release.all(:limit => 2, :order => 'created_at DESC')
+      releases = Release.previous(:limit => 2)
       distance_in_minutes = (((releases[0].created_at - releases[1].created_at).abs)/60)
       days = (distance_in_minutes / 1440)
       (self.tasks.features.verified.count / days * 7).to_i
@@ -42,7 +35,7 @@ class Release < ActiveRecord::Base
     end
   end
 
-  def can_complete_release?
+  def self.can_complete_release?
     !(Task.current.without_story.completed.any?)
   end
 
--- spec/models/release_spec.rb
+++ spec/models/release_spec.rb
@@ -2,56 +2,44 @@ require 'spec/spec_helper'
 
 describe Release do
   before do
-    @release = Factory(:release)
-    Factory(:task, :state => 'verified')
-    story = Factory(:story, :name => 'verified story')
-    Factory(:task, :story => story).update_attribute :state, 'verified'
-    story.state.should == 'verified'
-
-    story = Factory(:story, :name => 'incomplete story')
-    Factory(:task, :story => story).update_attribute :state, 'verified'
-    Factory(:task, :story => story).update_attribute :state, 'started'
-    story.state.should == 'started'
-
+    task = Factory(:task)
+    task.update_attribute(:state, 'verified')
     @developer = Factory(:developer)
   end
+
   describe 'finish release' do
     it 'should finish release' do
       ReleaseMailer.should_receive(:deliver_release_notification).and_return(true)
 
-      @release.finish!(@developer).should be_true
-      @release.reload
-      @release.stories.size.should == 1
-      @release.tasks.size.should == 1
+      Release.finish!(@developer).should be_true
+      release = Release.last
+      release.tasks.size.should == 1
     end
+
     it 'should not finish release if has unverified tasks' do
       Factory(:task).update_attribute :state, 'completed'
 
-      @release.finish!(@developer).should be_false
+      Release.finish!(@developer).should be_false
     end
+
     it 'should include invalid tasks' do
       task = Factory(:task)
       task.update_attribute :state, 'verified'
       invalid_task = Factory(:task)
       invalid_task.update_attribute :state, 'invalid'
 
-      @release.finish!(@developer).should be_true
-      @release.reload
-      @release.tasks.should include(task, invalid_task)
+      Release.finish!(@developer).should be_true
+      Release.last.tasks.should include(task, invalid_task)
     end
   end
 
   it 'should calculate velocity' do
-    release = Factory(:release)
     task = Factory(:task)
-    task.category = 'feature'
-    task.state = 'verified'
-    task.save!
-    release.tasks << task
-    release.finished_at = Time.now
-    release.created_at = Time.now - 1.day
-    release.save!
+    task.update_attribute(:state, 'verified')
+    Factory(:release, :created_at => Time.now - 2.days)
+    Factory(:release, :tasks => [task], :created_at => Time.now - 1.day)
 
-    release.velocity.round.should == 7
+    Release.velocity.should == 7
   end
 end
+
