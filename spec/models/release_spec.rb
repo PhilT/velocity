cc8fa7476104d2cae7f63411799c2a41b9613709
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2010-07-04 10:18:44 +0100

    bugfix: velocity for previous releases was incorrectly calculated

--- spec/models/release_spec.rb
+++ spec/models/release_spec.rb
@@ -50,5 +50,17 @@ describe Release do
 
     Release.velocity.should == 7
   end
+
+  it 'calculates velocity for a previous release' do
+    task = Factory(:task, :state => 'verified')
+    tasks = []
+    5.times {tasks << Factory(:task, :state => 'verified')}
+    
+    Factory(:release, :created_at => Time.now - 3.days)
+    release = Factory(:release, :tasks => [task], :created_at => Time.now - 2.day)
+    Factory(:release, :tasks => tasks, :created_at => Time.now)
+
+    release.velocity.should == 7
+  end
 end
 
