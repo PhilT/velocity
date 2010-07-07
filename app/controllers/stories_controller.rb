021a6e47763fb979b4ae2c5847bb81f4bbd58804
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2010-07-07 20:32:23 +0100

    removed unnecessary code from stories_controller, tested what was left

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,21 +1,10 @@
 class StoriesController < ApplicationController
-  def new
-    @story = Story.new
-  end
-
   def create
     @group = Story.new(params[:story])
     if @group.save
       respond_to do|format|
         format.js{render :layout => false}
       end
-    end
-  end
-
-private
-  def render_story
-    respond_to do|format|
-      format.js{render :action => :update, :layout => false}
     end
   end
 end
