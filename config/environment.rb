fb4026b525fecdb2e2f0a73deff1ebcedf0c4d67
Phil Thompson <phil@electricvisions.com>
2010-07-13 22:25:49 +0100

    switched to bundler, removed db2s3

--- config/environment.rb
+++ config/environment.rb
@@ -7,13 +7,7 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'aasm'
-  config.gem 'acts_as_list'
-  config.gem 'authlogic'
-  config.gem 'db2s3'
-  config.gem 'haml'
-  config.gem 'whenever', :lib => false
-  config.gem 'redirect_routing'
+
 end
 
 Sass::Plugin.options[:template_location] = RAILS_ROOT + '/app/styles' if defined?(Sass)
