596c24230e3367cb0926605221083d05ce453a6e
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2010-12-01 16:20:33 +0000

    activate previously created groups instead of creating them again

--- app/models/story.rb
+++ app/models/story.rb
@@ -4,13 +4,17 @@ class Story < ActiveRecord::Base
 
   default_scope :order => :name
   named_scope :created, lambda {{:conditions => ["created_at > ?", Task.last_poll]}}
-  named_scope :current, :conditions => ['release_id IS NULL']
+  named_scope :current, :conditions => {:active => true}
   named_scope :orphaned, :conditions => ['id NOT IN (SELECT story_id FROM tasks WHERE story_id IS NOT NULL)']
 
-  validates_uniqueness_of :name, :scope => :release_id
+  validates_uniqueness_of :name
 
   def self.remove_orphans
     orphaned.destroy_all
+  end
+
+  def activate
+    self.active = true
   end
 end
 
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,13 +1,21 @@
 require 'spec_helper'
 
 describe 'Group' do
-  describe 'orphaned' do
+  describe '#orphaned' do
     it 'should remove stories not assigned to any tasks' do
       story = Factory(:story)
       task = Factory(:task, :story => Factory(:story))
       task_without_story = Factory(:task)
       story2 = task.story
       lambda{Story.remove_orphans}.should change(Story, :count).by -1
+    end
+  end
+
+  describe '#activate' do
+    it 'activates a group' do
+      group = Factory.build(:story, :active => false)
+      group.activate
+      group.active?.should be_true
     end
   end
 end
